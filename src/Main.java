6de063193a787e78cedd5e7268cd49d6fa0c87db
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-15 03:29:00 +0530

    Initial commit done

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+/*
+You are building a library data management app. You are supposed to create three entities: library item, book, and magazine.
+The library item should have the following attributes:
+id (integer)
+title (string)
+year (integer)
+ book  should inherit from the library item class and have an additional attribute:
+author (string)
+magazine should also inherit from the library item class and have an additional attribute:
+issue (integer)
+Create a java code for the above requirements and demonstrate its functionalities.
+*/
+
+package day3;
+
+class libraryItem{
+    private int id;
+
+    public void setId(int newId){
+        id=newId;
+    }
+    public int getId(){
+        return id;
+    }
+
+    String title;
+    int year;
+}
+
+class book extends libraryItem {
+    String author;
+
+    public void GetInfo(){
+        System.out.println("Title\tYear\tAuthor");
+        System.out.println(title+"\t"+year+"\t"+author);
+    }
+}
+
+class magazine extends libraryItem{
+    int issue;
+
+    public void GetInfo(){
+        System.out.println("Title\t\tYear\tIssue");
+        System.out.println(title+"\t"+year+"\t"+issue);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // Book
+        book newComic = new book();
+        newComic.setId(12);
+        newComic.author="Alan Moore";
+        newComic.title="Batman";
+        newComic.year=1988;
+        System.out.println("ID : "+newComic.getId());
+        newComic.GetInfo();
+
+        // Magazine
+        magazine newIndian = new magazine();
+        newIndian.setId(25);
+        newIndian.title="Times Now";
+        newIndian.issue=2;
+        newIndian.year=2023;
+        System.out.println("ID : "+newIndian.getId());
+        newIndian.GetInfo();
+    }
+
+
+}
